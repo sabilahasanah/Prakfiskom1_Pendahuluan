5b726fff6736ee04f2c032fbb32bcd6830246517
sabilahasanah <144420921+sabilahasanah@users.noreply.github.com>
2023-09-22 13:49:41 +0700

    Add files via upload

---
+++ Soal_Phy.py
@@ -0,0 +1,9 @@
+# Menentukan Gaya yang bekerja pada benda dengan Hukum II Newton
+m = 4 # Kg
+a = 20 # m/s^2
+
+# Rumus Hukum II Newton
+F = m*a
+
+# Menampilkan hasil
+print("Gaya yang diberikan pada benda adalah", F, "Newton")
